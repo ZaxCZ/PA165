1dc3c8815e926dfc25b05c8f4101a64ab14294a9
Petr Valenta <xvalent4@nymfe33.fi.muni.cz>
2018-10-02 13:10:39 +0200

    mocking rate table + assertj tests

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,24 +1,91 @@
 package cz.muni.fi.pa165.currency;
 
 import org.junit.Test;
+import org.mockito.internal.matchers.Null;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class CurrencyConvertorImplTest {
 
+
+
     @Test
     public void testConvert() {
+
+        Currency eur = Currency.getInstance("EUR");
+        Currency czk = Currency.getInstance("CZK");
+        Currency usd = Currency.getInstance("USD");
+
+        ExchangeRateTable mockExchangeRateTable = mock(ExchangeRateTable.class);
+
+        when(mockExchangeRateTable.getExchangeRate(czk, usd).thenReturn(new BigDecimal("0.50")));
+        when(mockExchangeRateTable.getExchangeRate(usd, czk).thenReturn(new BigDecimal("2")));
+
+        when(mockExchangeRateTable.getExchangeRate(eur, czk).thenReturn(new BigDecimal("25.2929")));
+        when(mockExchangeRateTable.getExchangeRate(czk, eur).thenReturn(new BigDecimal("0.1499")));
+
+        when(mockExchangeRateTable.getExchangeRate(usd, eur).thenReturn(new BigDecimal("-2.000001")));
+        when(mockExchangeRateTable.getExchangeRate(eur, usd).thenReturn(new BigDecimal("0")));
+
+        CurrencyConvertorImpl testedConvertor = new CurrencyConvertorImpl(mockExchangeRateTable);
+
+
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1")).isEqualTo(new BigDecimal("2")));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("5")).isEqualTo(new BigDecimal("10")));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1.86")).isEqualTo(new BigDecimal("3.72")));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1.999")).isEqualTo(new BigDecimal("4")));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("0.009")).isEqualTo(new BigDecimal("0")));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("0")).isEqualTo(new BigDecimal("0")));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("-5")).isEqualTo(new BigDecimal("-10")));
+
+
+        assertThat(testedConvertor.convert(czk, usd, new BigDecimal("1")).isEqualTo(new BigDecimal("0.50")));
+
+/*
+        assertThat(testedConvertor.convert(czk, eur, new BigDecimal("10")).isEqualTo(new BigDecimal("26545")));
+        assertThat(testedConvertor.convert(eur, czk, new BigDecimal("137.41")).isEqualTo(new BigDecimal("26545")));
+
         // Don't forget to test border values and proper rounding.
         fail("Test is not implemented yet.");
+*/
+
     }
 
     @Test
     public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+        //given
+        Currency czk = Currency.getInstance("CZK");
+
+        ExchangeRateTable mockExchangeRateTable = mock(ExchangeRateTable.class);
+
+        CurrencyConvertorImpl testedConvertor = new CurrencyConvertorImpl(mockExchangeRateTable);
+
+        //expect
+        Throwable thrown = catchThrowable(() -> { testedConvertor.convert(null, czk, new BigDecimal("1"));});
+
+        //when
+        assertThat(thrown).hasMessageContaining("IllegalArgumentException");
     }
 
     @Test
     public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
+        //given
+        Currency czk = Currency.getInstance("CZK");
+
+        ExchangeRateTable mockExchangeRateTable = mock(ExchangeRateTable.class);
+
+        CurrencyConvertorImpl testedConvertor = new CurrencyConvertorImpl(mockExchangeRateTable);
+
+        //expect
+        Throwable thrown = catchThrowable(() -> { testedConvertor.convert(czk, null, new BigDecimal("1"));});
+
+        //when
+        assertThat(thrown).hasMessageContaining("IllegalArgumentException");
     }
 
     @Test
