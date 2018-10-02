dd5d451931244a3a41d6bb8763a3c4edd0b2d9e9
Petr Valenta <xvalent4@nymfe33.fi.muni.cz>
2018-10-02 13:31:25 +0200

    fixed brackets in test methods

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -24,27 +24,27 @@ public class CurrencyConvertorImplTest {
     public void testConvert() throws ExternalServiceFailureException {
 
 
-        when(mockExchangeRateTable.getExchangeRate(czk, usd)).thenReturn(new BigDecimal("0.50")));
-        when(mockExchangeRateTable.getExchangeRate(usd, czk)).thenReturn(new BigDecimal("2")));
+        when(mockExchangeRateTable.getExchangeRate(czk, usd)).thenReturn(new BigDecimal("0.50"));
+        when(mockExchangeRateTable.getExchangeRate(usd, czk)).thenReturn(new BigDecimal("2"));
 
-        when(mockExchangeRateTable.getExchangeRate(eur, czk)).thenReturn(new BigDecimal("25.2929")));
-        when(mockExchangeRateTable.getExchangeRate(czk, eur)).thenReturn(new BigDecimal("0.1499")));
+        when(mockExchangeRateTable.getExchangeRate(eur, czk)).thenReturn(new BigDecimal("25.2929"));
+        when(mockExchangeRateTable.getExchangeRate(czk, eur)).thenReturn(new BigDecimal("0.1499"));
 
-        when(mockExchangeRateTable.getExchangeRate(usd, eur)).thenReturn(new BigDecimal("-2.000001")));
-        when(mockExchangeRateTable.getExchangeRate(eur, usd)).thenReturn(new BigDecimal("0")));
-
-
-
-        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1"))).isEqualTo("2"));
-        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("5"))).isEqualTo(new BigDecimal("10")));
-        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1.86"))).isEqualTo(new BigDecimal("3.72")));
-        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1.999"))).isEqualTo(new BigDecimal("4")));
-        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("0.009"))).isEqualTo(new BigDecimal("0")));
-        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("0"))).isEqualTo(new BigDecimal("0")));
-        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("-5"))).isEqualTo(new BigDecimal("-10")));
+        when(mockExchangeRateTable.getExchangeRate(usd, eur)).thenReturn(new BigDecimal("-2.000001"));
+        when(mockExchangeRateTable.getExchangeRate(eur, usd)).thenReturn(new BigDecimal("0"));
 
 
-        assertThat(testedConvertor.convert(czk, usd, new BigDecimal("1"))).isEqualTo(new BigDecimal("0.50")));
+
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1"))).isEqualTo("2");
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("5"))).isEqualTo(new BigDecimal("10"));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1.86"))).isEqualTo(new BigDecimal("3.72"));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("1.999"))).isEqualTo(new BigDecimal("4"));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("0.009"))).isEqualTo(new BigDecimal("0"));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("0"))).isEqualTo(new BigDecimal("0"));
+        assertThat(testedConvertor.convert(usd, czk, new BigDecimal("-5"))).isEqualTo(new BigDecimal("-10"));
+
+
+        assertThat(testedConvertor.convert(czk, usd, new BigDecimal("1"))).isEqualTo(new BigDecimal("0.50"));
 
 /*
         assertThat(testedConvertor.convert(czk, eur, new BigDecimal("10")).isEqualTo(new BigDecimal("26545")));
@@ -85,7 +85,7 @@ public class CurrencyConvertorImplTest {
 
     @Test
     public void testConvertWithUnknownCurrency() throws ExternalServiceFailureException {
-        when(mockExchangeRateTable.getExchangeRate(gel, czk)).thenReturn(null));
+        when(mockExchangeRateTable.getExchangeRate(gel, czk)).thenReturn(null);
 
         //expect
         Throwable thrown = catchThrowable(() -> { testedConvertor.convert(gel, czk,  new BigDecimal("1"));});
@@ -96,7 +96,7 @@ public class CurrencyConvertorImplTest {
 
     @Test
     public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
-        when(mockExchangeRateTable.getExchangeRate(gel, usd)).thenThrow(ExternalServiceFailureException.class));
+        when(mockExchangeRateTable.getExchangeRate(gel, usd)).thenThrow(ExternalServiceFailureException.class);
 
 
         //expect
