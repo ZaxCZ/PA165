929415ff01cbe5e915f12e4aacf09587a27925fb
Petr Valenta <xvalent4@nymfe33.fi.muni.cz>
2018-10-23 13:22:26 +0200

    task03

---
+++ eshop-persistence/src/main/java/cz/fi/muni/pa165/dao/ProductDaoImpl.java
@@ -0,0 +1,61 @@
+package cz.fi.muni.pa165.dao;
+
+import java.util.List;
+
+
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import cz.fi.muni.pa165.entity.Product;
+
+@Repository
+public class ProductDaoImpl implements ProductDao {
+    @PersistenceContext
+    private EntityManager em;
+
+
+    @Override
+    public void create(Product product) {
+        em.persist(product);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return em.createQuery("select p from Product p", Product.class)
+                .getResultList();
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return em.find(Product.class, id);
+    }
+
+    @Override
+    public void remove(Product p) throws IllegalArgumentException{
+        em.remove(findById(p.getId()));
+    }
+
+
+    @Override
+    public List <Product> findByName(String name){
+        if (name == null || name.isEmpty())
+            throw new IllegalArgumentException("Cannot search for null e-mail");
+
+        try {
+            return em
+                    .createQuery("select p from Product p where name=:name",
+                            Product.class).setParameter("name", name)
+                    .getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+}
+
+
+
+
+
